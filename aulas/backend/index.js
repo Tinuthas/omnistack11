a708139b3f13ca470fa8b36fd5feeebe4e458af7
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-24 22:44:44 -0300

    res with json return

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    return res.send('hello world')
+    return res.json({
+        evento: 'Omnistack 11',
+        aluno: 'Vinicius'
+    })
 })
 
 app.listen(3333)
