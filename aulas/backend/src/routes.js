b626d638ee4ef53688b5672b0b4f3f108646275e
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-25 09:08:17 -0300

    get all ongs by selected

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -3,6 +3,11 @@ const crypto = require('crypto')
 const connection = require('./database/connection')
 const routes = express.Router()
 
+routes.get('/ongs', async (req, res) => {
+    const ongs = await connection('ongs').select('*')
+    return res.json(ongs)
+})
+
 
 routes.post('/ongs', async (req, res) => {
     const { name, email, whatsapp, city, uf} = req.body
