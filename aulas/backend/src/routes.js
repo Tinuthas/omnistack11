adf5e586c71dcdcb6066d3ce352734d30447606a
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-25 09:43:01 -0300

    route incidents success

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -7,6 +7,6 @@ routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
 
 routes.post('/incidents', IncidentController.create)
-routes.post('/incidents', IncidentController.index)
+routes.get('/incidents', IncidentController.index)
 
 module.exports = routes
