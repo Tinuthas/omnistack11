f2db95144c8530a481ed89e52c44fa40a2d277e7
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-26 19:38:02 -0300

    start logon page

--- aulas/frontend/src/pages/Logon/index.js
+++ aulas/frontend/src/pages/Logon/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import './styles.css'
+import heroesImg from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg'
 
 export default function Logon() {
     return (
-        <h1>Hello World</h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form action="">
+                    <h1>Faça seu login</h1>
+                    <input placeholder="Sua ID" type="text"/>
+                    <button type="submit">Entra</button>
+                </form>
+            </section>
+            <img src={heroesImg} alt="Heroes" />
+        </div>
     )
 }
