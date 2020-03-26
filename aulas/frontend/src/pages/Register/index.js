55fcdad4aadddf8b0eddaaa1cc278644b4bf75db
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-26 20:56:12 -0300

    create styles to form register

--- aulas/frontend/src/pages/Register/index.js
+++ aulas/frontend/src/pages/Register/index.js
@@ -13,7 +13,7 @@ export default function Register() {
                     <img src={logoImg} alt="Be The Hero"/>
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
-                    <Link to="/register" className="back-link">
+                    <Link to="/" className="back-link">
                         <FiArrowLeft size={16} color="#E02041"/>
                         Não tenho cadastro
                      </Link>
