ca51058422c53b13e8f737431441b538435a7dc0
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-28 13:52:59 -0300

    create header mobile detail

--- aulas/mobile/src/pages/Detail/index.js
+++ aulas/mobile/src/pages/Detail/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import { View } from 'react-native'
+import {Feather} from '@expo/vector-icons'
+import { View, Image,  TouchableOpacity } from 'react-native'
+
+import styles from './styles'
+import logoImg from '../../assets/logo.png'
 
 export default function Detail() {
     return (
-        <View />
+        <View style={styles.container}>
+            <View style={styles.header}> 
+                <Image source={logoImg} />
+                <TouchableOpacity>
+                    <Feather name="arrow-left" size={28} color="#E02041"/>
+                </TouchableOpacity>
+            </View>
+
+
+        </View>
     )
 }
