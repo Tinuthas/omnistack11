56d77682e17ec06caee84e8312b94e43e887e79b
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-27 23:41:30 -0300

    create inputs cases

--- aulas/frontend/src/pages/NewIncident/index.js
+++ aulas/frontend/src/pages/NewIncident/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
 
@@ -6,6 +6,9 @@ import './styles.css'
 import logoImg from '../../assets/logo.svg'
 
 export default function NewIncident() {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState('')
     return (
         <div className="new-incident-container">
         <div className="content">
@@ -19,9 +22,22 @@ export default function NewIncident() {
                  </Link>
             </section>
             <form action="">
-                <input placeholder="Titulo do Caso" />
-                <textarea type="email" placeholder="Descrição" />
-                <input placeholder="Valor em reais"/>
+                <input 
+                    placeholder="Titulo do Caso"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+
+                />
+                <textarea type="email" 
+                    placeholder="Descrição" 
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                />
+                <input 
+                    placeholder="Valor em reais"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
                 <button className="button" type="submit">Cadastrar</button>
             </form>
         </div>
