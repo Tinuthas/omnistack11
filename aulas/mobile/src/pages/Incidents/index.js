bd1fa812f3f65fe98cf771fd15fd35baae1da897
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-28 13:46:27 -0300

    navigate to detail

--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { View, FlatList, Image, Text, TouchableOpacity} from 'react-native'
 import {Feather} from '@expo/vector-icons'
+import {useNavigation} from '@react-navigation/native'
 
 import logoImg from '../../assets/logo.png'
 import styles from './styles'
 
 export default function Incidents() {
+
+    const navigation = useNavigation()
+
+    function navigateToDetail() {
+        navigation.navigate('Detail')
+    }
     return (
         <View style={styles.container}> 
             <View style={styles.header}> 
@@ -34,7 +41,7 @@ export default function Incidents() {
                         <Text style={styles.incidentProperty}>VALOR:</Text>
                         <Text style={styles.incidentValue}>APAD</Text>
 
-                        <TouchableOpacity style={styles.detailsButton} onPress={()=> {}}>
+                        <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                             <Feather name="arrow-right" size={16} color="#E03041" />
                         </TouchableOpacity>
