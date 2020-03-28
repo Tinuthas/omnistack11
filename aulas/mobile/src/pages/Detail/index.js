21326d1157ab0f910e6db1ee6280fe8cd1a29193
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-28 14:30:11 -0300

    styles to box detail

--- aulas/mobile/src/pages/Detail/index.js
+++ aulas/mobile/src/pages/Detail/index.js
@@ -13,9 +13,10 @@ export default function Detail() {
                 <TouchableOpacity>
                     <Feather name="arrow-left" size={28} color="#E02041"/>
                 </TouchableOpacity>
+            </View>
 
-                <View style={styles.incident}>
-                    <Text style={styles.incidentProperty}>ONG:</Text>
+            <View style={styles.incident}>
+                    <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
                     <Text style={styles.incidentValue}>APAD</Text>
 
                     <Text style={styles.incidentProperty}>CASO:</Text>
@@ -39,9 +40,6 @@ export default function Detail() {
                         </TouchableOpacity>
                     </View>
                 </View>
-            </View>
-
-
         </View>
     )
 }
