0a09ed90e9034d12ee78831ffc4e84fcc105acc1
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-03-28 15:40:15 -0300

    show total

--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -11,10 +11,12 @@ export default function Incidents() {
 
     const navigation = useNavigation()
     const [incidents, setIncidents] = useState([])
+    const [total, setTotal] = useState(0)
 
     async function loadIncidents() {
         const response = await api.get('incidents')
         setIncidents(response.data)
+        setTotal(response.headers['x-total-count'])
     }
     useEffect(() => {
        loadIncidents() 
@@ -28,7 +30,7 @@ export default function Incidents() {
             <View style={styles.header}> 
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View>
             <Text style={styles.title}>Bem-vindo!</Text>
